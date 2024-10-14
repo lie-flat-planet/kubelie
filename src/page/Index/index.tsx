8a99f3bf28bcf344053a03dd5e82f2814d4c90f2
xiaoxlm <39261724+xiaoxlm@users.noreply.github.com>
2024-10-14 10:45:03 +0800

    ++

--- src/page/Index/index.tsx
+++ src/page/Index/index.tsx
@@ -10,15 +10,19 @@ import DynamicBreadcrumb from './DynamicBreadcrumb';
 const Index = () => {
   return (
     <Layout style={{ minHeight: '100vh', borderRadius: '10px 0px 0px' }}>
+      {/* 侧边栏 */}
       <SiderMenu />
 
       <Layout>
+        {/* 顶部 */}
         <TopHeader />
+        {/* 面包屑 */}
         <div className="mt-1 ml-3">
-          {/* 面包屑 */}
           <DynamicBreadcrumb />
         </div>
+        {/* 内容 */}
         <Cnt />
+        {/* 底部 */}
         <BottomFooter />
       </Layout>
     </Layout>
